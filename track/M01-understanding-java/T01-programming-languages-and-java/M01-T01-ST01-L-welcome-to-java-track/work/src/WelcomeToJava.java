0a2ba6f64238ab230372b5420893db5621dd7cbb
aswin017c <aswin017c@gmail.com>
2026-07-30 18:41:40 +0530

    day1changes1

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,7 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
         System.err.println("welcome to java world");
+        System.out.println("i know some of the javastuff");
+        System.out.println("i am very excited to learn java");
     }
 }
